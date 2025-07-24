28043d54b69367e301343082fc82fef31b7fcc4f
Arnab Mukhopadhyay <arnabmukh008@gmail.com>
2025-07-25 00:42:50 +0530

    testing

--- terraform/environments/test/vars.tf
+++ terraform/environments/test/vars.tf
@@ -1,68 +1,81 @@
 
 variable "project_name" {
   description = "The name of the project."
-  type        = mc_iac_eks
+  type        = string
+  default     = mc_iac_eks
 }
 
 variable "environment" {
   description = "Environment name"
-  type        = test
+  type        = string
+  default     = test
 }
 
 variable "aws_region" {
   description = "AWS Region"
-  type        = ap-south-1
+  type        = string
+  default     = ap-south-1
 }
 
 
 variable "vpc_cidr" {
   description = "VPC CIDR Network"
-  type        = 10.1.0.0/16
+  type        = string
+  default     = 10.1.0.0/16
 }
 
 variable "public_subnet_cidrs" {
   description = "Public Subnet CIDR"
-  type        = ["10.1.1.0/24", "10.1.2.0/24"]
+  type        = list(string)
+  default = ["10.1.1.0/24", "10.1.2.0/24"]
 }
 
 variable "private_subnet_cidrs" {
   description = "Private Subnet CIDR"
-  type        = ["10.1.10.0/24", "10.1.11.0/24"]
+  type        = list(string)
+  default     = ["10.1.10.0/24", "10.1.11.0/24"]
 }
 
 variable "cluster_name" {
   description = "Name of the EKS Cluster"
-  type        = mc-iac-eks-cluster
+  type        = string
+  default     = mc-iac-eks-cluster
 }
 
 variable "kubernetes_version" {
   description = "Kubernetes Version"
-  type        = 1.33
+  type        = string
+  default     = 1.33
 }
 
 variable "instance_type" {
   description = "Instance Type"
-  type        = t3.large
+  type        = string
+  default     = t3.large
 }
 
 
 variable "desired_size" {
   description = "Desired Size"
-  type        = 2
+  type        = string
+  default = 2
 }
 
 variable "max_size" {
   description = "Max Size"
-  type        = 3
+  type        = string
+  default = 3
 }
 
 variable "min_size" {
   description = "Min Size"
-  type        = 2
+  type        = string
+  default = 2
 }
 
 variable "aws_cli_user_name" {
   description = "AWS CLI User Name"
-  type        = your-aws-cli-user-name
+  type        = string
+  default = AKIARGWBZATJJDOXV4FY
 }
 
