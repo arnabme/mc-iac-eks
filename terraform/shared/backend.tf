45b386d3ec1033897a1b59ce5d1b00ceb334aaf7
Arnab Mukhopadhyay <arnabmukh008@gmail.com>
2025-07-25 01:48:39 +0530

    give me a break

--- terraform/shared/backend.tf
+++ terraform/shared/backend.tf
@@ -1,7 +1,18 @@
-backend "s3" {
-    bucket         = "mc-ias-eks" # Replace with your S3 bucket name
-    key            = "mc-iac-eks"
-    region         = "eu-north-1" # Or your desired default region
+terraform {
+  backend "s3" {
+    bucket         = "mc-ias-eks"
+    key            = "mc-ias-eks"
+    region         = "eu-north-1"
     encrypt        = true
-    dynamodb_table = "mc-iac-eks" # Replace with your DynamoDB table name (must have LockID as primary key)
-    }
+    dynamodb_table = "mc-iac-ekse" # Optional DynamoDB table for state locking
+  }
+}
+
+
+# backend "s3" {
+#     bucket         = "mc-ias-eks" # Replace with your S3 bucket name
+#     key            = "mc-iac-eks"
+#     region         = "eu-north-1" # Or your desired default region
+#     encrypt        = true
+#     dynamodb_table = "mc-iac-eks" # Replace with your DynamoDB table name (must have LockID as primary key)
+# }
